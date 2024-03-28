e75b374dffdb5fb37a534cbecce4563435357089
Ngranados314 <noah.granados314@gmail.com>
2024-03-28 00:06:24 -0500

    Empty controller function

---
+++ src/main/java/com/ScheduleSync/ScheduleSync/createAccountController.java
@@ -0,0 +1,12 @@
+package com.ScheduleSync.ScheduleSync;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+
+public class createAccountController {
+    public static void createAccount(String userID, String password){
+        
+    }
+
+}
