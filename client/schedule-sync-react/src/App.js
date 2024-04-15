5a54604ce173c067304cecc877c382176eb04e80
sugarglider717 <sugarglider717@gmail.com>
2024-04-15 12:24:46 -0500

    Changes

--- client/schedule-sync-react/src/App.js
+++ client/schedule-sync-react/src/App.js
@@ -1,76 +1,23 @@
-import { useEffect } from "react";
-import {
-  Routes,
-  Route,
-  useNavigationType,
-  useLocation,
-} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import LogIn from "./pages/LogIn";
-import ForgotPassword from "./pages/ForgotPassword";
-import CreateAccount from "./pages/CreateAccount";
-import UserDashboard from "./pages/UserDashboard";
-import GroupSchedule from "./pages/GroupSchedule";
+import ForgotPassword from './pages/ForgotPassword'
+import CreateAccount from './pages/CreateAccount'
+import UserDashboard from './pages/UserDashboard'
+import GroupSchedule from './pages/GroupSchedule'
 
-function App() {
-  const action = useNavigationType();
-  const location = useLocation();
-  const pathname = location.pathname;
-
-  useEffect(() => {
-    if (action !== "POP") {
-      window.scrollTo(0, 0);
-    }
-  }, [action, pathname]);
-
-  useEffect(() => {
-    let title = "";
-    let metaDescription = "";
-
-    switch (pathname) {
-      case "/":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/forgot-password":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/create-account":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/user-dashboard":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/group-schedule":
-        title = "";
-        metaDescription = "";
-        break;
-    }
-
-    if (title) {
-      document.title = title;
-    }
-
-    if (metaDescription) {
-      const metaDescriptionTag = document.querySelector(
-        'head > meta[name="description"]'
-      );
-      if (metaDescriptionTag) {
-        metaDescriptionTag.content = metaDescription;
-      }
-    }
-  }, [pathname]);
-
+export default function App() {
+  
   return (
+    <div>
+      
     <Routes>
       <Route path="/" element={<LogIn />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/create-account" element={<CreateAccount />} />
-      <Route path="/user-dashboard" element={<UserDashboard />} />
-      <Route path="/group-schedule" element={<GroupSchedule />} />
+      <Route path="/ForgotPassword" element={<ForgotPassword />} />
+      <Route path="/CreateAccount" element={<CreateAccount />} />
+      <Route path="/UserDashboard" element={<UserDashboard />} />
+      <Route path="/GroupSchedule" element={<GroupSchedule />} />
     </Routes>
-  );
+    
+    </div>
+  )
 }
-export default App;
